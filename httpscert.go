ec47d969d81bc7cee36c84d09984ca31072906b5
Takafumi Ono <ono_pm@amber.plala.or.jp>
2016-05-04 02:40:01 +0900

    set InsecureSkipVerify true

--- httpscert.go
+++ httpscert.go
@@ -1,6 +1,7 @@
 package httpscert
 
 import (
+	"crypto/tls"
 	"fmt"
 	"net/http"
 )
@@ -9,7 +10,17 @@ func Run(url string) error {
 
 	infof("http get: %s", url)
 
-	resp, err := http.Get(url)
+	infof("set InsecureSkipVerify: true")
+	//not set TLS.VerifiedChains.
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+			MinVersion:         0,
+		},
+	}
+	client := &http.Client{Transport: tr}
+
+	resp, err := client.Get(url)
 	if err != nil {
 		critf("http handle error: %s", err)
 		return fmt.Errorf("http get error")
@@ -22,7 +33,17 @@ func Run(url string) error {
 
 		for i := 0; i < len(resp.TLS.PeerCertificates); i++ {
 			infof("cert[%d]: %s", i, resp.TLS.PeerCertificates[i].Subject.CommonName)
+
+			if i == 0 {
+				err := resp.TLS.PeerCertificates[i].VerifyHostname(resp.Request.URL.Host)
+				if err != nil {
+					warnf("cert[%d]: %s", i, err)
+				}
+
+			}
+
 			printCert(resp.TLS.PeerCertificates[i], fmt.Sprintf("  cert[%d]", i))
+
 		}
 	} else {
 		warnf("not found TLS information.")
